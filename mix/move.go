78e427a861ed2a22bc973002cc6dbe84e0cba550
Justin Flude <jflude@users.noreply.github.com>
2020-06-02 11:32:29 -0500

    ensure nothing happens with MOVE when f == 0

--- mix/move.go
+++ mix/move.go
@@ -1,6 +1,9 @@
 package mix
 
 func (c *Computer) move(aa Word, i, f, op, m int) int64 {
+	if f == 0 {
+		return 1
+	}
 	to := c.Reg[I1].Int()
 	if m < 0 || m+f >= MemorySize ||
 		to < 0 || to+f >= MemorySize {
